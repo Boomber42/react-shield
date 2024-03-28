c2ff2a6396ee4e95545ec10200a804a9b564032a
Boomber42 <cleiton.-.mar@outlook.com>
2024-03-28 14:45:04 -0300

    putting the title objects/vehicle in the top

--- src/components/cardItens.tsx
+++ src/components/cardItens.tsx
@@ -10,10 +10,10 @@ export default function CardItens(props: CardProps) {
     return (
         <div>
             <div className="card">
+                <h2 className="titulo">{props.title}</h2>
                 <Link to={'objects'}>
                     <img className="image" src={props.image} alt={props.imageAlt} />
                 </Link>
-                <h2 className="titulo">{props.title}</h2>
             </div>
         </div>
     )
