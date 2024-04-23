59e4b23d686cf51bdcaa3eb9ba08b258a376528c
barravitor <edson.vitor42@gmail.com>
2024-04-23 19:44:28 -0300

    feat: add zod on project n config on create subjects

--- src/components/customModal/customModal.tsx
+++ src/components/customModal/customModal.tsx
@@ -7,6 +7,9 @@ import Loading from '../loading/loading';
 import ImageCropper from '../imageCropper';
 import { toast, Bounce } from "react-toastify";
 import Toast from "../toast/toast";
+import { z } from "zod";
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 const customStyles = {
   content: {
@@ -28,13 +31,27 @@ interface CustomModalProps {
 }
 
 export default function CustomModal(props: CustomModalProps) {
-  let [title, setTitle] = useState<string>('');
-  let [name, setName] = useState<string>('');
-  let [codeName, setCodeName] = useState<string>('');
-  let [status, setStatus] = useState<string>('');
   let [image, setImage] = useState<string>('');
   let [loading, setLoading] = useState(false);
   let [cropOpen, setCropOpen] = useState(false);
+
+  const createSubjectFormSchema = z.object({
+    title: z.string().min(1, { message: 'Este campo é obrigatório' }).default(''),
+    name: z.lazy(() => {
+      return ['Agente', 'Vingador'].includes(props.typeModal) ? z.string().min(1, { message: 'Este campo é obrigatório' }).default('') : z.undefined();
+    }),
+    codeName: z.lazy(() => {
+      return ['Agente', 'Vingador'].includes(props.typeModal) ? z.string().min(1, { message: 'Este campo é obrigatório' }).default('') : z.undefined();
+    }),
+    status: z.lazy(() => {
+      return ['Agente', 'Vingador'].includes(props.typeModal) ? z.string().min(1, { message: 'Este campo é obrigatório' }).default('') : z.undefined();
+    })
+  });
+
+  const { register, handleSubmit, reset, formState: { errors, isValid } } = useForm<z.infer<typeof createSubjectFormSchema>>({
+    mode: "all",
+    resolver: zodResolver(createSubjectFormSchema),
+  });
 
   let subtitle: any;
 
@@ -42,20 +59,28 @@ export default function CustomModal(props: CustomModalProps) {
     subtitle.style.color = 'rgb(72, 28, 72)';
   }
 
-  async function submitForm() {
-    try{
+  async function submitForm(data: any) {
+    const response = createSubjectFormSchema.safeParse(data);
+
+    if (!response.success || !image) {
+      return
+    }
+
+    try {
       setLoading(true);
 
       var form: any = {
-        title,
-        name,
-        codeName,
-        status,
-        image,
-        alt: `Imagem do ${name}`,
-        type: props.typeModal
-      };
-  
+        ...data,
+        ...{
+          image,
+          alt: `Imagem do ${data.name}`,
+          type: props.typeModal
+        }
+      }
+
+      reset();
+      setImage('');
+
       const api = new Api();
       await api.postSubject(form as Subject);
       setLoading(false);
@@ -71,8 +96,7 @@ export default function CustomModal(props: CustomModalProps) {
         transition: Bounce,
       });
       props.closeModal(true);
-    }
-    catch(error){
+    } catch(error) {
       setLoading(false);
       toast.error('Erro no cadasto!', {
         position: "top-right",
@@ -112,6 +136,12 @@ export default function CustomModal(props: CustomModalProps) {
     setCropOpen(false);
   }
 
+  const closeModal = (): void => {
+    reset();
+    setImage('');
+    props.closeModal();
+  }
+
   return (
     <div>
       <Toast/>
@@ -131,42 +161,51 @@ export default function CustomModal(props: CustomModalProps) {
               <div>Preencha o formulário com as informações</div>
 
               <div className='form'>
-                <label htmlFor="titulo"> Titulo </label>
-                <input type='text' placeholder='Titulo' id='titulo' onChange={(event) => setTitle(event.target.value)} />
+                <label htmlFor="title"> Titulo </label>
+                <input type='text' placeholder='Titulo' id='titulo' {...register("title", { required: true })} />
+                {errors.title && <span>{errors.title.message}</span>}
 
                 {['Agente', 'Vingador'].includes(props.typeModal) ? (
                   <>
-                    <label htmlFor="nome"> Nome </label>
-                    <input type='text' placeholder='Nome' id='nome' onChange={(event) => setName(event.target.value)} />
+                    <label htmlFor="name"> Nome </label>
+                    <input type='text' placeholder='Nome' id='nome' {...register("name", { required: true })} />
+                    {errors.name && <span>{errors.name.message}</span>}
                   </>
                 ) : null}
 
                 {['Agente', 'Vingador'].includes(props.typeModal) ? (<>
-                  <label htmlFor="codinome"> Codinome </label>
-                  <input type='text' placeholder='Codinome' id='codinome' onChange={(event) => setCodeName(event.target.value)} />
+                  <label htmlFor="codeName"> Codenome </label>
+                  <input type='text' placeholder='Codinome' id='codinome' {...register("codeName", { required: true })} />
+                  {errors.codeName && <span>{errors.codeName.message}</span>}
                 </>) : null}
 
                 {['Agente', 'Vingador'].includes(props.typeModal) ? (<>
                   <label htmlFor="status"> Status </label>
-                  <input type='text' placeholder='Status' id='status' onChange={(event) => setStatus(event.target.value)} />
+                  <input  type='text' placeholder='Status' id='status' {...register("status", { required: true })} />
+                  {errors.status && <span>{errors.status.message}</span>}
                 </>) : null}
 
                 {['Veiculo', 'Objeto'].includes(props.typeModal) ? (<>
                   <label> Adicione a imagem </label>
                   <input type='file' name='imagem' accept='image/*' onChange={onSelectFile} />
+                  {!isValid || !image ? <span>Este campo é obrigatório</span> : null}
                 </>) : null}
               </div>
             </>
           )}
 
-          {['Agente', 'Vingador'].includes(props.typeModal) ? (<>
+          {['Agente', 'Vingador'].includes(props.typeModal) ? (<div style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}>
             <ImageCropper setCropStatus={setCropStatus} closeImageModal={closeImageModal} />
-          </>) : null}
+            {!isValid || !image ? <span>Este campo é obrigatório</span> : null}
+          </div>) : null}
 
           {!cropOpen && (
             <div className='buttonStyle'>
-              <Button name='Salvar' onClick={submitForm} loading={loading} />
-              <Button name='Fechar' onClick={props.closeModal} loading={loading} />
+              <Button name='Salvar' onClick={handleSubmit(submitForm)} loading={loading} />
+              <Button name='Fechar' onClick={closeModal} loading={loading} />
             </div>
           )}
 
